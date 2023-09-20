f07887002abd0c5f0a5d117786ced2ac35eec007
Danin Sadžak <daninsadzak@gmail.com>
2023-09-20 23:05:23 +0200

    Started working on the returnTenseVerb() method

--- 1stconjugation.hpp
+++ 1stconjugation.hpp
@@ -1,5 +1,35 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <string>
+
+class LatinVerb {
+    public:
+        std::string returnTenseVerb(std::string verb);
+};
+
+std::string LatinVerb::returnTenseVerb(std::string verb) {
+    return verb;
+}
 
 void conjugate1st() {
-    std::cout << "Yay it worked!\n";
+    system("CLS");
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 12);
+    std::cout << "You chose the 1st conjugation!\n";
+    Sleep(1000);
+    system("CLS");
+
+    std::cin.ignore();
+
+    LatinVerb v;
+
+    std::string verb;
+    std::cout << "Enter in a verb in Latin!\n";
+    std::getline(std::cin, verb);
+    
+    std::cout << '\n';
+
+    std::cout << "Output:\n";
+    std::cout << v.returnTenseVerb(verb);
 }
