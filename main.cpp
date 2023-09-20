15f64fa0367b0430062018dfc4d179fbb8e5e309
Danin Sadžak <daninsadzak@gmail.com>
2023-09-20 22:42:25 +0200

    Added beginning text, and declared the 1st conjugation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 
 int main() {
-    std::cout << "Hello, World!\n";
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, 10);
+    std::cout << "LATIN CONJUGATION\n";
+    std::cout << '\n\n';
+    std::cout << "Press any button to continue\n";
+    if(getch()) {
+        Beep(500, 400);
+    }
+    SetConsoleTextAttribute(h, 7);
     return 0;
 }
