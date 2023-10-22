3858cfeb00819589c31ec71504001a0176af2205
Danin Sadžak <daninsadzak@gmail.com>
2023-10-22 12:38:53 +0200

    Set up conjugation methods.

---
+++ conjugation.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class LatinVerb {
+    public:
+        std::string getTenseVerb(std::string verb);
+        std::string getBaseVerb(std::string verb);
+};
