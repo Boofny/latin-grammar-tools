63c01fe37c9a82b509620f9550fdf85fbd2c6422
Danin Sadžak <daninsadzak@gmail.com>
2023-10-22 13:39:08 +0200

    Finished with implementing the first declination

--- declination.h
+++ declination.h
@@ -6,10 +6,10 @@
 class LatinNoun {
     public:
         std::string noun;
-        std::string getBaseNoun(std::string noun);
+        std::string getBaseNoun1st(std::string noun);
 };
 
-std::string LatinNoun::getBaseNoun(std::string noun) {
+std::string LatinNoun::getBaseNoun1st(std::string noun) {
     return noun.erase(noun.length() - 1);
 }
 
@@ -20,5 +20,23 @@ void declinate1st() {
     std::cin.ignore();
     std::getline(std::cin, n.noun);
 
-    std::cout << n.getBaseNoun(n.noun) << '\n';
+    std::cout << '\n';
+
+    std::cout << "Singularis:\n";
+    std::cout << "N   " << n.getBaseNoun1st(n.noun).append("a") << '\n';
+    std::cout << "G   " << n.getBaseNoun1st(n.noun).append("ae") << '\n';
+    std::cout << "D   " << n.getBaseNoun1st(n.noun).append("ae") << '\n';
+    std::cout << "Acc " << n.getBaseNoun1st(n.noun).append("am") << '\n';
+    std::cout << "V   " << n.getBaseNoun1st(n.noun).append("a") << '\n';
+    std::cout << "Abl " << n.getBaseNoun1st(n.noun).append("a") << '\n';
+
+    std::cout << '\n';
+
+    std::cout << "Pluralis:\n";
+    std::cout << "N   " << n.getBaseNoun1st(n.noun).append("ae") << '\n';
+    std::cout << "G   " << n.getBaseNoun1st(n.noun).append("arum") << '\n';
+    std::cout << "D   " << n.getBaseNoun1st(n.noun).append("is") << '\n';
+    std::cout << "Acc " << n.getBaseNoun1st(n.noun).append("as") << '\n';
+    std::cout << "V   " << n.getBaseNoun1st(n.noun).append("ae") << '\n';
+    std::cout << "Abl " << n.getBaseNoun1st(n.noun).append("is") << '\n';
 }
